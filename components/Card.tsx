1226ee0a1d233424f0841bf4a347236824385e39
Tejas Sohoni <tjs1597@gmail.com>
2022-01-09 14:59:59 +0530

    Initial Commit

---
+++ components/Card.tsx
@@ -0,0 +1,5 @@
+export default function Card() {
+  return (
+    <div className="flex flex-row border-0 rounded-lg shadow-md py-5 px-5"></div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+    colors: {
+      mainbg: "	#0d192b",
+      cardbg: "	#14253d",
+      line: "	#2f415b",
+      footer: "hsl(215, 51%, 70%)",
+    },
   },
   plugins: [],
-}
+};
